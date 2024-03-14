82b450dd6e3b9b983e652886eab0db9dc75c700b
Adib Kurniawan <adibkurniawan00@gmail.com>
2024-03-15 03:56:57 +0700

    tugas akhir mobile automation

--- test/specs/mobile.automation.js
+++ test/specs/mobile.automation.js
@@ -1,5 +1,12 @@
-describe('Open Application',() => {
-    it('should open apps', async() => {
-        await $("//android.widget.TextView[@content-desc='Accessibility']").click();
+describe("Welcome Page",()=>{
+    it("add user success", async()=>{
+        await $("id:com.fghilmany.dietmealapp:id/et_name").addValue("sumirnop")
+        await $("id:com.fghilmany.dietmealapp:id/et_weight").addValue("62")
+        await $("id:com.fghilmany.dietmealapp:id/et_height").addValue("178")
+        await $("id:com.fghilmany.dietmealapp:id/rb_male").click()
+        await $('//android.widget.Button[@resource-id="com.fghilmany.dietmealapp:id/bt_next"]').click()
+        await $("id:com.fghilmany.dietmealapp:id/rb_rest").click()
+        await $("id:com.fghilmany.dietmealapp:id/bt_finish").click()
+        await expect($('(//android.widget.ImageView[@resource-id="com.fghilmany.dietmealapp:id/navigation_bar_item_icon_view"])[1]')).toBeDisplayed()
     })
 })
